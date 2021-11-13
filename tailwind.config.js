cd3a2c25d85f4e392446ce62455ea6063739d47c
Ahmed <arizkcisco@gmail.com>
2021-11-13 19:26:26 +0200

    install tailwind and firebase

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'floral-white': '#f9f6f1',
+      },
+      screens: {
+        tablet: { min: '678px', max: '1023px' },
+      },
+      fontSize: {
+        '3.5xl': [
+          '2rem',
+          {
+            lineHeight: '2.25rem',
+          },
+        ],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
